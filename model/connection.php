94f09e659999bc384d000914e1f998b5be541d93
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-13 01:06:52 +0200

    Add connection

---
+++ model/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "sgd";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
+
+?>
