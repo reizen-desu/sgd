92fd7d569a1cba4e7cfc12b8167f1a2359cab023
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-14 03:00:00 +0200

    Fix: Mal posicionamento do modal footer

--- index.php
+++ index.php
@@ -25,68 +25,7 @@
     <br>
     <br>
 
-    <div class="container" id="diarista-cards">
-        <div class="row row-cols-1 row-cols-xxl-6 row-cols-xl-5 row-cols-lg-5 row-cols-md-3 row-cols-sm-2">
-            <!-- g-2 g-lg-3 g-xl-4 -->
-            <div class="col">
-                <div class="card">
-                    <img src="img/logo.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h6 class="card-title text-center">Ana Paula, F, 35 anos</h6>
-                        <div class="card-text fst-italic">Estou sempre pronta pra trabalhar, ligue e vamos falar!
-                        </div>
-                        <br>
-                        <div>
-                            <b>Bairro</b>: Maxaquene C<br>
-                            <b>Especialidade:</b>limpeza<br>
-                            <b>Disponibilidade:</b> 2ª a 6ª feira<br>
-                            <b>Contacto:</b> 8x xxx-xxxx
-                        </div>
-                        <br>
-                        <a href="#" class="btn btn-outline-primary">Ver perfil completo</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                    <img src="img/logo.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h6 class="card-title text-center">Anabela Macuacua, F, 26 anos</h6>
-                        <div class="card-text fst-italic">Estou sempre pronta pra trabalhar, ligue e vamos falar!
-                        </div>
-                        <br>
-                        <div>
-                            <b>Bairro</b>: Aeroporto B<br>
-                            <b>Especialidade:</b> babá<br>
-                            <b>Disponibilidade:</b> 2ª a 3ª feira<br>
-                            <b>Contacto:</b> 8x xxx-xxxx
-                        </div>
-                        <br>
-                        <a href="#" class="btn btn-outline-primary">Ver perfil completo</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                    <img src="img/logo.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h6 class="card-title text-center">Sergio Nhanala, M, 20 anos</h6>
-                        <div class="card-text fst-italic">Faço trabalhos de limpeza profundos (produtos químicos)
-                        </div>
-                        <br>
-                        <div>
-                            <b>Bairro</b>: Alto-maé<br>
-                            <b>Especialidade:</b>limpeza
-                            <b>Disponibilidade:</b> 2ª feira
-                            <b>Contacto:</b> 8x xxx-xxxx
-                        </div>
-                        <br>
-                        <a href="#" class="btn btn-outline-primary">Ver perfil completo</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php require_once 'view/cartas.php'; ?>
 
     <!-- Formulario de login -->
     <?php require_once 'view/formulario_login.php'; ?>
