3716838ba2f69cbcaba8aa3c1b2f0cee292217ef
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-16 23:14:01 +0200

    Create tela_principal.php

---
+++ view/tela_principal.php
@@ -0,0 +1,3 @@
+<!-- Esta é a tela de início, quando a diarista nem o visitante fizeram o login.
+    Irá mostrar as funcionalidades do sistema como também alguns perfis ilustrativos.
+    Uma espécie de landing page. -->
