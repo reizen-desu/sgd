f75993550102d4d36ba47043a2bf935ea101ccff
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-16 20:12:08 +0200

    Controle de registro para diarista e visitante

---
+++ controller/registerController.php
@@ -0,0 +1,14 @@
+<?php
+require '../model/connection.php';
+
+$pdo = conectar();
+
+// Para registar a diarista
+if (isset($_POST['registar_diarista'])) {
+
+}
+
+// Para registar o visitante
+if (isset($_POST['registar_visitante'])) {
+    
+}
