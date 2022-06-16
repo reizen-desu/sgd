375174e528186d64b44206d36b5c21b314f32c38
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-16 20:26:05 +0200

    Movido código de footer para footer.php

--- index.php
+++ index.php
@@ -53,14 +53,8 @@
 
 
     <!-- RODAPÉ -->
-    <footer class="text-center">
-        <!-- Texto de copyright -->
-        <div id="copyright-text" class="text-center text-white-50 text-center">
-            © 2022 Copyright:
-            <a href="https://github.com/reizen-desu/" target="_blank" class="text-white-50">Reizen</a>
-        </div>
-    </footer>
-    <!-- FIM DO RODAPÉ -->
+    <?php require_once 'view/footer.php'; ?>
+
 
     <script src="assets/js/functions.js"></script>
     <!-- Bootstrap JavaScript Libraries -->
--- view/footer.php
+++ view/footer.php
@@ -1,1 +1,7 @@
-<!-- NOT FILLED YET -->
+<footer class="text-center">
+    <!-- Texto de copyright -->
+    <div id="copyright-text" class="text-center text-white-50 text-center">
+        © 2022 Copyright:
+        <a href="https://github.com/reizen-desu/" target="_blank" class="text-white-50">Reizen</a>
+    </div>
+</footer>
