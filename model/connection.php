5b44adfaf6b4dd7c91580010325189dea217502f
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-16 20:01:13 +0200

    Adicionar função conectar para iniciar a base de dados

--- model/connection.php
+++ model/connection.php
@@ -7,11 +7,15 @@ $user = "root";
 $pass = "";
 $db = "sgd";
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Erro: " . $e->getMessage();
+function conectar() {
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Erro: " . $e->getMessage();
+    }
+
+    return $pdo;
 }
 
 ?>
