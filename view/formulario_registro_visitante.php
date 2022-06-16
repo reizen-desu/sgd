f8163e1ad13e7722480b1762a2742ffc84d2f726
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-16 20:11:32 +0200

    Adicionar informações de submissão

--- view/formulario_registro_visitante.php
+++ view/formulario_registro_visitante.php
@@ -1,1 +1,11 @@
-<h2>To add</h2>
+<div class="text-muted">
+    Registando como um visitante, você poderá requisitar algum serviço à diaristas e poderá publicar seus serviços.
+</div>
+
+<form action="../controller/registerController.php">
+
+    <hr>
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <button type="submit" value="registar_visitante" class="btn btn-primary btn-block">Registrar nova conta</button>
+    </div>
+</form>
