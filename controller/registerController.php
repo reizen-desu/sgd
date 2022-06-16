4944e95b2d0d07aeac1cd740307f1830a1de06f9
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-16 23:12:50 +0200

    Adicionado variaveis de registro de diarista

--- controller/registerController.php
+++ controller/registerController.php
@@ -5,7 +5,17 @@ $pdo = conectar();
 
 // Para registar a diarista
 if (isset($_POST['registar_diarista'])) {
-
+    $nome = $_POST['nome'];
+    $apelido = $_POST['apelido'];
+    $email = $_POST['emailLogin'];
+    $password = $_POST['password'];
+    // $password_confirm = $_POST['password_confirm'];
+    $data_nascimento = $_POST['data_nascimento'];
+    $telemovel = $_POST['contacto'];
+    $sexo = $_POST['sexo'];
+    $morada = $_POST['morada'];
+    $descricao = $_POST['descricao'];
+    $is_public = $_POST['is_public'];
 }
 
 // Para registar o visitante
