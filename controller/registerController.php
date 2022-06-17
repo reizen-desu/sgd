7847017463c43ad2f6fecf7c95ee7558f2dee53e
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-17 07:52:29 +0200

    Inicialização das variáveis submitidas

--- controller/registerController.php
+++ controller/registerController.php
@@ -1,24 +1,69 @@
 <?php
-require '../model/connection.php';
+include '../model/connection.php';
 
 $pdo = conectar();
 
 // Para registar a diarista
 if (isset($_POST['registar_diarista'])) {
+    $tipo_usuario = "diarista";
     $nome = $_POST['nome'];
     $apelido = $_POST['apelido'];
-    $email = $_POST['emailLogin'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
     // $password_confirm = $_POST['password_confirm'];
     $data_nascimento = $_POST['data_nascimento'];
-    $telemovel = $_POST['contacto'];
+    $contacto = $_POST['contacto'];
     $sexo = $_POST['sexo'];
     $morada = $_POST['morada'];
     $descricao = $_POST['descricao'];
     $is_public = $_POST['is_public'];
+
+    $result = $pdo->prepare("INSERT INTO diarista (nome, apelido, email, password, data_nascimento, contacto, sexo, morada, descricao, is_public) VALUES (:nome, :apelido, :email, :password, :data_nascimento, :contacto, :sexo, :morada, :descricao, :is_public)");
+    $result->bindParam(':nome', $nome);
+    $result->bindParam(':apelido', $apelido);
+    $result->bindParam(':email', $email);
+    $result->bindParam(':password', $password);
+    $result->bindParam(':data_nascimento', $data_nascimento);
+    $result->bindParam(':contacto', $contacto);
+    $result->bindParam(':sexo', $sexo);
+    $result->bindParam(':morada', $morada);
+    $result->bindParam(':descricao', $descricao);
+    $result->bindParam(':is_public', $is_public);
+    
+    if ($result->execute()) {
+        echo "<script>alert('Registo efetuado com sucesso!')</script>";
+        header("Location: ../");
+    } else {
+        echo "<script>alert('Erro ao registar!')</script>";
+    }
 }
 
 // Para registar o visitante
 if (isset($_POST['registar_visitante'])) {
+    $tipo_usuario = "visitante";
+    $nome = $_POST['nome'];
+    $apelido = $_POST['apelido'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    // $password_confirm = $_POST['password_confirm'];
+    $contacto = $_POST['contacto'];
+    $morada = $_POST['morada'];
+
+    $result = $pdo->prepare("INSERT INTO visitante (nome, apelido, email, password, contacto, morada)
+    VALUES (:nome, :apelido, :email, :password, :contacto, :morada)");
     
+    $result->bindParam(':nome', $nome);
+    $result->bindParam(':apelido', $apelido);
+    $result->bindParam(':email', $email);
+    $result->bindParam(':password', $password);
+    $result->bindParam(':contacto', $contacto);
+    $result->bindParam(':morada', $morada);
+
+    if ($result->execute()) {
+        echo "<script>alert('Registo efetuado com sucesso!')</script>";
+        header("Location: ../");
+    } else {
+        echo "<script>alert('Erro ao registar!')</script>";
+    }
+
 }
