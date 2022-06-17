49346f9d5518798e6456e02e0d6f520916af637f
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-17 07:53:01 +0200

    Fix: varíaveis não sendo reconhecidas fora do scirpt

--- model/connection.php
+++ model/connection.php
@@ -8,6 +8,7 @@ $pass = "";
 $db = "sgd";
 
 function conectar() {
+    global $host, $user, $pass, $db;
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
