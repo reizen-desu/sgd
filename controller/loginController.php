71ef1f7edacd1d441b0eb643cd1507da0c4d64ca
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-22 00:19:16 +0200

    Codigo de validação de credenciais

--- controller/loginController.php
+++ controller/loginController.php
@@ -2,7 +2,32 @@
 session_start();
 include '../model/connection.php';
 
-$pdo = conectar();
+$_SESSION['is_logged'] = false;
+$_SESSION['tipo_usuario'] = '';
 
-$sql = "SELECT * FROM (SELECT email, password, tipo_usuario FROM visitante UNION
-SELECT email, password, tipo_usuario FROM diarista) WHERE email = :email AND password = :password";
+
+
+
+$pdo = conectar();
+// Validar as credenciais
+
+$sql = "SELECT id, email, password, tipo_usuario FROM visitante UNION
+SELECT id, email, password, tipo_usuario FROM diarista WHERE email = :email AND password = :password";
+
+if($stmt = $pdo->prepare($sql)) {
+    $stmt->bindParam(':email', $_POST['email']);
+    $stmt->bindParam(':password', $_POST['password']);
+
+    if($stmt->rowCount() == 1) {
+        $row = $stmt->fetch();
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['email'] = $row['email'];
+        $_SESSION['tipo_usuario'] = $row['tipo_usuario'];
+        $_SESSION['is_logged'] = true;
+        header("Location: ../");
+    } else {
+        echo "<script>alert('Email ou password incorretos!')</script>";
+        header("Location: ../");
+    }
+
+}
