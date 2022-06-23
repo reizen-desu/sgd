ea4d0fde6bb5f09872bb1fe1cbe871227b197d49
reizen-desu <reizen.dos.santos@gmail.com>
2022-06-23 02:58:59 +0200

    Two functions to both register forms

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,14 +1,31 @@
-// Verifica se as senhas são iguais
-var password = document.getElementById("password"), confirm_password = document.getElementById("confirm_password");
+// Verifica se as senhas são iguais para Diarista
+var password_diarista = document.getElementById("password_diarista"), confirm_password_diarista = document.getElementById("confirm_password_diarista");
 
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("As senhas não correspondem");
-  } else {
-    confirm_password.setCustomValidity('');
-  }
+function validatePasswordDiarista(){
+    if(password_diarista.value != confirm_password_diarista.value) {
+        confirm_password_diarista.setCustomValidity("As senhas não correspondem");
+    } else {
+        confirm_password_diarista.setCustomValidity('');
+    }
 }
 
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+
+password_diarista.onchange = validatePasswordVisitante;
+confirm_password_diarista.onkeyup = validatePasswordVisitante;
+
+
+
+// Verifica se as senhas são iguais para Visitantes
+var password_visitante = document.getElementById("password_visitante"), confirm_password_visitante = document.getElementById("confirm_password_visitante");
+
+function validatePasswordVisitante(){
+    if(password_visitante.value != confirm_password_visitante.value) {
+      confirm_password_visitante.setCustomValidity("As senhas não correspondem");
+    } else {
+      confirm_password_visitante.setCustomValidity('');
+    }
+  }
+
+password_visitante.onchange = validatePasswordVisitante;
+confirm_password_visitante.onkeyup = validatePasswordVisitante;
 
